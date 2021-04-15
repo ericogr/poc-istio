c93fb7fc5ae9cbae981c9707c2ed52090311895c
EricoGR <ericogr@gmail.com>
2021-04-15 14:19:34 -0300

    modularizar e adicionar espera para deploy de componentes

--- install.sh
+++ install.sh
@@ -21,13 +21,42 @@ PROMETHEUS_NAMESPACE=prometheus-operator
 ISTIO_NAMESPACE=istio-system
 ISTIO_BIN=bin/istio-$ISTIO_VERSION/bin/istioctl
 
+create_namespace() {
+  PARAM_NAMESPACE=$1
+  echo '->'Criando namespace $PARAM_NAMESPACE
+  kubectl create ns $PARAM_NAMESPACE --dry-run=client -oyaml|kubectl apply -f -
+}
+
+wait_rollout() {
+  PARAM_NAMESPACE=$1
+  PARAM_DEPLOYMENT=$2
+  PARAM_RETRIES=$3
+  echo "->Aguarda rollout do $PARAM_NAMESPACE/$PARAM_DEPLOYMENT - $PARAM_RETRIES tentativas"
+
+  for i in $(seq $PARAM_RETRIES); do
+    err=0
+    kubectl rollout status deployment -n $PARAM_NAMESPACE $PARAM_DEPLOYMENT || err=$?
+
+    if [ $err -eq 0 ]; then
+      echo "->Istiod OK"
+      break
+    else
+      echo "->Erro, tentando novamente $i de $PARAM_RETRIES"
+      sleep 5
+    fi
+  done
+}
+
 # download do istio
 if [ ! -f $ISTIO_BIN ]; then
+  echo '->'Download do Istio
   curl -L https://istio.io/downloadIstio | sh -
   if [ ! -d $DIR/bin ]; then
+    echo '->'Criando diretório de destino do istioctl
     mkdir $DIR/bin
   fi
 
+  echo '->'Movendo o istioctl para sua pasta bin
   mv istio-$ISTIO_VERSION bin
 fi
 
@@ -38,13 +67,15 @@ fi
 # -----------------------
 # instalar metrics server
 # -----------------------
+echo '->'Instalando o metrics server
 kubectl -n kube-system apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl -n kube-system patch deployment metrics-server --patch "$(cat metrics-server-patch.yaml)"
 
 # -------------------------------
 # instalar operator do prometheus
 # -------------------------------
-kubectl create ns $PROMETHEUS_NAMESPACE --dry-run=client -oyaml|kubectl apply -f -
+echo '->'Instalando o Prometheus operator
+create_namespace $PROMETHEUS_NAMESPACE
 helm upgrade \
   --install \
   -n $PROMETHEUS_NAMESPACE \
@@ -56,6 +87,7 @@ helm upgrade \
 # instalar operator do istio
 # --------------------------
 # instalação
+echo '->'Instalando o Istio operator
 $ISTIO_BIN operator init --operatorNamespace istio-operator
 
 # ---------------------
@@ -63,9 +95,10 @@ $ISTIO_BIN operator init --operatorNamespace istio-operator
 # ---------------------
 
 # criar namespace
-kubectl create ns $ISTIO_NAMESPACE --dry-run=client -oyaml | kubectl apply -f -
+create_namespace $ISTIO_NAMESPACE
 
 # instalar com profile demo com istio operator
+echo '->'Configurar o Istio usando o operator com perfil demo
 kubectl apply -f - <<EOF
 apiVersion: install.istio.io/v1alpha1
 kind: IstioOperator
@@ -76,21 +109,24 @@ spec:
   profile: demo
 EOF
 
+# aguardar rollout do operator
+wait_rollout $ISTIO_NAMESPACE istiod 5
+
 # --------------------------
 # configurar a demo do istio
 # --------------------------
 
 # configurar o namespace da demo
-echo configurar o namespace da demo
-kubectl create ns $DEMO_NAMESPACE --dry-run=client -oyaml | kubectl apply -f -
+create_namespace $DEMO_NAMESPACE
+echo '->'Habilitar o namespace da demonstração para o Istio
 kubectl label --overwrite=true namespace $DEMO_NAMESPACE istio-injection=enabled
 
 # instalar a demo
-echo Instalando a demo bookinfo
+echo '->' Instalando a demo bookinfo
 kubectl apply -n $DEMO_NAMESPACE -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/platform/kube/bookinfo.yaml
 
 # configurar o ingress gateway da demo
-echo Configurar o ingress gateway da demo
+echo '->' Configurar o ingress gateway da demo
 kubectl apply -n $DEMO_NAMESPACE -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/bookinfo-gateway.yaml
 
 # descobrir portas e IP's (configurado para kubernetes kind)
@@ -100,9 +136,10 @@ TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o j
 INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
 GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 
-echo Demo disponível em: http://$GATEWAY_URL/productpage
+echo '->' Demo disponível em: http://$GATEWAY_URL/productpage
 
 # aplicar regras de destino
+echo '->'Aplicar regras adicionais na demo do Istio
 kubectl apply -n $DEMO_NAMESPACE -f https://raw.githubusercontent.com/istio/istio/release-1.9/samples/bookinfo/networking/destination-rule-all.yaml
 
 # -----------------------------
@@ -110,6 +147,7 @@ kubectl apply -n $DEMO_NAMESPACE -f https://raw.githubusercontent.com/istio/isti
 # -----------------------------
 
 # criar o service monitoring
+echo '->'Configurar o monitoramento do Prometheus para o Istio
 kubectl -n $ISTIO_NAMESPACE apply -f https://raw.githubusercontent.com/istio/istio/4461a6b2324bceabd6f0ef3896ca1ca338180c45/samples/addons/extras/prometheus-operator.yaml
 kubectl -n $ISTIO_NAMESPACE label --overwrite -f https://raw.githubusercontent.com/istio/istio/4461a6b2324bceabd6f0ef3896ca1ca338180c45/samples/addons/extras/prometheus-operator.yaml release=prometheus
 
@@ -127,7 +165,8 @@ kubectl -n $ISTIO_NAMESPACE label --overwrite -f https://raw.githubusercontent.c
 # instalação padrão do kiali
 # --------------------------
 # se o script abaixo falhar, tente rodar novamente depois de alguns segundos
-kubectl create ns $KIALI_NAMESPACE --dry-run=client -oyaml | kubectl apply -f -
+create_namespace $KIALI_NAMESPACE
+echo '->'Instalação do Kiali
 helm upgrade \
     --install \
     --set cr.create=true \
@@ -136,6 +175,7 @@ helm upgrade \
     --repo https://kiali.org/helm-charts \
     kiali-operator \
     kiali-operator
+echo '->'Configuração do Kiali com o operator
 kubectl -n $KIALI_NAMESPACE apply -f kiali.yaml
 
 # executar o kiali: istioctl dashboard kiali
